bae1ed315fb151e932a6ac578fb1a010de834b8b
Julio César <jcesarmobile@gmail.com>
2021-12-04 23:55:06 +0100

    feat: Add iOS support

--- ios/Plugin/SslSkipPlugin.m
+++ ios/Plugin/SslSkipPlugin.m
@@ -1,8 +1,37 @@
-#import <Foundation/Foundation.h>
-#import <Capacitor/Capacitor.h>
+#import <WebKit/WebKit.h>
+#import <objc/message.h>
+#import <objc/runtime.h>
 
-// Define the plugin using the CAP_PLUGIN Macro, and
-// each method the plugin supports using the CAP_PLUGIN_METHOD macro.
-CAP_PLUGIN(SslSkipPlugin, "SslSkip",
-           CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
-)
+@implementation NSObject (WKNavigationDelegate)
+ 
++ (void)load {
+  static dispatch_once_t onceToken;
+  dispatch_once(&onceToken, ^{
+    Class class = [self class];
+    SEL originalSelector = @selector(webView:didReceiveAuthenticationChallenge:completionHandler:);
+    SEL swizzledSelector = @selector(SkipSSLwebView:didReceiveAuthenticationChallenge:completionHandler:);
+
+    Method originalMethod = class_getInstanceMethod(class, originalSelector);
+    Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+
+    BOOL didAddMethod = class_addMethod(class,
+                                        originalSelector,
+                                        method_getImplementation(swizzledMethod),
+                                        method_getTypeEncoding(swizzledMethod));
+    if (didAddMethod) {
+      class_replaceMethod(class,
+                          swizzledSelector,
+                          method_getImplementation(originalMethod),
+                          method_getTypeEncoding(originalMethod));
+    } else {
+      method_exchangeImplementations(originalMethod, swizzledMethod);
+    }
+  });
+}
+
+- (void)SkipSSLwebView:(WKWebView *)webView didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge completionHandler:(void (^)(NSURLSessionAuthChallengeDisposition disposition, NSURLCredential *credential))completionHandler{
+  NSURLCredential * credential = [[NSURLCredential alloc] initWithTrust:[challenge protectionSpace].serverTrust];
+  completionHandler(NSURLSessionAuthChallengeUseCredential, credential);
+}
+
+@end
