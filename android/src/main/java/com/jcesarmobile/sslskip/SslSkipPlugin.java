86bdfcf2de1dc85d83bbb855667958828764de86
jcesarmobile <jcesarmobile@gmail.com>
2025-12-15 01:10:21 +0100

    feat!: Capacitor 7 support

--- android/src/main/java/com/jcesarmobile/sslskip/SslSkipPlugin.java
+++ android/src/main/java/com/jcesarmobile/sslskip/SslSkipPlugin.java
@@ -3,16 +3,13 @@ package com.jcesarmobile.sslskip;
 import android.net.http.SslError;
 import android.webkit.SslErrorHandler;
 import android.webkit.WebView;
-
 import com.getcapacitor.BridgeWebViewClient;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.annotation.CapacitorPlugin;
-
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
-
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
@@ -23,39 +20,50 @@ import javax.net.ssl.X509TrustManager;
 @CapacitorPlugin(name = "SslSkip")
 public class SslSkipPlugin extends Plugin {
 
-    TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
-        public X509Certificate[] getAcceptedIssuers() {
-            return new X509Certificate[]{};
-        }
+    TrustManager[] trustAllCerts = new TrustManager[] {
+        new X509TrustManager() {
+            public X509Certificate[] getAcceptedIssuers() {
+                return new X509Certificate[] {};
+            }
 
-        @Override
-        public void checkClientTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
-            // Not implemented
-        }
+            @Override
+            public void checkClientTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+                // Not implemented
+            }
 
-        @Override
-        public void checkServerTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
-            // Not implemented
+            @Override
+            public void checkServerTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+                // Not implemented
+            }
         }
-    }};
+    };
 
     public void load() {
         try {
             SSLContext sc = SSLContext.getInstance("TLS");
             sc.init(null, trustAllCerts, new java.security.SecureRandom());
             HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() { @Override public boolean verify(String hostname, SSLSession session) { return true; } });
+            HttpsURLConnection.setDefaultHostnameVerifier(
+                new HostnameVerifier() {
+                    @Override
+                    public boolean verify(String hostname, SSLSession session) {
+                        return true;
+                    }
+                }
+            );
             HttpsURLConnection.setFollowRedirects(true);
         } catch (KeyManagementException e) {
             e.printStackTrace();
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
-        this.bridge.setWebViewClient(new BridgeWebViewClient(this.bridge) {
-            @Override
-            public void onReceivedSslError(WebView view, final SslErrorHandler handler, SslError error) {
-                handler.proceed();
+        this.bridge.setWebViewClient(
+            new BridgeWebViewClient(this.bridge) {
+                @Override
+                public void onReceivedSslError(WebView view, final SslErrorHandler handler, SslError error) {
+                    handler.proceed();
+                }
             }
-        });
+        );
     }
 }
