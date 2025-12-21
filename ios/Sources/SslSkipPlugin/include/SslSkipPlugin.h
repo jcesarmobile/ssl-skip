b31572bf418cdec77fbc392b25aa416ad365260e
jcesarmobile <jcesarmobile@gmail.com>
2025-12-21 01:44:18 +0100

    feat: Add SPM support (#17)

---
+++ ios/Sources/SslSkipPlugin/include/SslSkipPlugin.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for Plugin.
+FOUNDATION_EXPORT double PluginVersionNumber;
+
+//! Project version string for Plugin.
+FOUNDATION_EXPORT const unsigned char PluginVersionString[];
